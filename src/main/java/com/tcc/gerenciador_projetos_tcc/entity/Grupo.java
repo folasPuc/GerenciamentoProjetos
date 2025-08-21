de89a378bad79dffcf72308c193e04332250a0d3
rafaelsato <rafael.rsato@gmail.com>
2025-08-20 22:10:17 -0300

    bug fix

--- src/main/java/com/tcc/gerenciador_projetos_tcc/entity/Grupo.java
+++ src/main/java/com/tcc/gerenciador_projetos_tcc/entity/Grupo.java
@@ -14,7 +14,7 @@ public class Grupo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String nome;
 
     @Column(nullable = false)
